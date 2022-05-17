f96e79d3ab853050b402a3cf35ad4d1dc774f035
anonymousdomain <dawityitagesu@gmail.com>
2022-05-17 23:09:13 +0300

    firestore playground #1fecting data

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:familyshare/widgets/header.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+final db = FirebaseFirestore.instance;
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,9 +12,24 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    Firebase.initializeApp();
+    super.initState();
+    getUsers();
+  }
+
+  getUsers() {
+    db.collection('users').get().then((QuerySnapshot snapshot) {
+      snapshot.docs.forEach((DocumentSnapshot doc) {
+        print(doc.data());
+      });
+    });
+  }
+
+  @override
   Widget build(context) {
     return Scaffold(
-      appBar: header(context,isAppTitle: true),
+      appBar: header(context, isAppTitle: true),
       body: Text('Time line'),
     );
   }
