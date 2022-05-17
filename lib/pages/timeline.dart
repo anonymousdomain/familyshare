bce3190f2821f7f559c68f45fb0edfa8071bdc2d
anonymousdomain <dawityitagesu@gmail.com>
2022-05-18 00:17:09 +0300

    fehing data using future + stream builder

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:familyshare/widgets/header.dart';
+import 'package:familyshare/widgets/progress.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-final db = FirebaseFirestore.instance;
+final CollectionReference db = FirebaseFirestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -15,24 +16,23 @@ class _TimelineState extends State<Timeline> {
   void initState() {
     Firebase.initializeApp();
     super.initState();
-    getUsers();
-  }
-
-  getUsers() async{
-   final QuerySnapshot snapshot= await db.collection('users').where('username',isEqualTo: 'dawitMekonnen').where('pageCount',isEqualTo: 3).get();
-    
-    snapshot.docs.forEach((DocumentSnapshot doc) {
-      print(doc.data());
-      print(doc.id);
-      print(doc.exists);
-    });
   }
 
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text('Time line'),
+      body: StreamBuilder<QuerySnapshot>(
+        stream:db.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          final  List<Text> children =
+              snapshot.data!.docs.map((doc) => Text(doc['username'])).toList();
+          return Container(child: ListView(children: children));
+        },
+      ),
     );
   }
 }
