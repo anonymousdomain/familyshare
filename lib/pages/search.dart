b8581eab1a61845e2ab4a373d296de9294c10370
anonymousdomain <dawityitagesu@gmail.com>
2022-05-21 22:25:18 +0300

    search layout

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,9 +7,57 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  AppBar buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        decoration: InputDecoration(
+          hintText: 'search for a user...',
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 30.0,
+          ),
+          suffixIcon: IconButton(
+            onPressed: () => print('cleard'),
+            icon: Icon(Icons.clear),
+          ),
+        ), 
+      ),
+    );
+  }
+
+  buildNoContent() {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            SvgPicture.asset('assets/images/search.svg',
+                height: orientation == Orientation.portrait ? 300 : 200),
+            Text(
+              'Find User',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.italic),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+      appBar: buildSearchField(),
+      body: buildNoContent(),
+    );
   }
 }
 
